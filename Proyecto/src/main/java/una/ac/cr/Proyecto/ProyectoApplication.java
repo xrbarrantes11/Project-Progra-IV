3411dddf742949486042634dd262d0dd105fbf57
xrbarrantes11 <randy.barrantes.pizarro@est.una.ac.cr>
2022-05-27 23:57:03 -0600

    Segundo commit

--- Proyecto/src/main/java/una/ac/cr/Proyecto/ProyectoApplication.java
+++ Proyecto/src/main/java/una/ac/cr/Proyecto/ProyectoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoApplication {
 
-    //primer commit
+    //segundo commit
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
